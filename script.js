edf47148f74df563fc1889243c2751c9be0b6980
Samplasion <ercolino2004@gmail.com>
2017-09-21 15:44:20 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,274 @@
+var deg = Math.PI / 180
+function Data() {
+  var d = new Date;
+  var day = d.getDay();
+  var dd = d.getDay();
+  dd += 1
+  if (day==0) {
+    day="Sunday"
+  } else if (day==1) {
+    day='Monday'
+  } else if (day==2) {
+    day='Tuesday'
+  } else if (day==3) {
+    day='Wednesday'
+  } else if (day==4) {
+    day='Thursday'
+  } else if (day==5) {
+    day='Friday'
+  } else {
+    day='Saturday'
+  };
+  var mday = d.getDate();
+  var month = d.getMonth();
+  month += 1;
+  var year = d.getYear();
+  year += 1900;
+  var yr = d.getYear();
+  yr -= 100
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  if (secs < 10) {
+    secs = '0'+secs
+  }
+  if (mins < 10) {
+    mins = '0'+mins
+  }
+  if (hours < 10) {
+    hours = '0'+hours
+  }
+  if (month < 10) {
+    month = '0'+month
+  };
+document.getElementById('newd').innerHTML =  hours+":"+mins+":"+secs+' '+day+', '+month+'/'+mday+'/'+year+" ('"+yr+')';
+  var bdayMsg = "Compleanno di ";
+  var mm = "Mamma"
+  var pp = "Papà"
+  var ff = "Faby"
+  var io = "Francy"
+  var bdayMsg2 = '! Oggi compie <font color="#f20">'
+  var mma = 1978
+  mma = year-mma
+  var ppa = 1970
+  ppa = year-ppa
+  var ffa = 2008
+  ffa = year-ffa
+  var ioa = 2004
+  ioa = year-ioa
+  if (mday==19 && month==6) {
+    var abc = document.getElementById('abc')
+abc.innerHTML = "--"+bdayMsg+mm+'! Oggi compierebbe <font color="#f20">'+mma+"</font> anni--";
+  }
+  if (mday==3 && month==2) {
+    var abc = document.getElementById('abc')
+abc.innerHTML = "--"+bdayMsg+pp+bdayMsg2+ppa+"</font> anni!--";
+  }
+  if (mday==25 && month==4) {
+    var abc = document.getElementById('abc')
+abc.innerHTML = "--"+bdayMsg+ff+bdayMsg2+ffa+"</font> anni!--";
+  }
+  if (mday==21 && month==4) {
+    var abc = document.getElementById('abc')
+abc.innerHTML = "--"+bdayMsg+io+'! Oggi compio <font color="#f20">'+ioa+"</font> anni!--";
+  }
+}
+function data() {
+  setInterval(Data, 1000)
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function random() {
+  var evenodd = '<font color="#090">odd</font>.'
+  let lol = getRndInteger(1, 1000)
+  var sel = document.getElementById("select")
+  if (lol%2==0) {
+    evenodd = '<font color="#00f">even</font>.'
+  }
+  var ll = document.getElementById('random')
+  ll.innerHTML = 'Your random number is <font color="#af5000">' + lol + '</font> and is ' + evenodd
+}
+
+function bdm() {
+  var d = new Date();
+  var mday = d.getDate();
+  var month = d.getMonth();
+  var bdayMsg = "Compleanno di Mamma";
+  if (mday==19 && month==6) {
+    document.getElementById('abc').innerHTML = "--"+bdayMsg+"--";
+  }
+}
+
+var img = [
+  'img/20170920225040.png',
+  'img/20170920230850.png',
+  "http://www.sololearn.com/uploads/slider/1.jpg", 
+  "http://www.sololearn.com/uploads/slider/2.jpg", 
+  "http://www.sololearn.com/uploads/slider/3.jpg"]
+var num = 0
+var photo = document.getElementById('photo')
+photo.src = img[num];
+
+function next() {
+  num += 1
+  if (num >= img.length) {
+    num = 0
+  }
+  photo.src = img[num];
+  console.log('Next ok')
+}
+
+function prev() {
+  num -= 1
+  if (num < 0) {
+    num = img.length - 1
+  }
+  photo.src = img[num];
+  console.log("Prev ok")
+}
+
+var img2 = [
+  'img/20170921084601.png',
+  'img/20170921084612.png']
+var num2 = 0
+var photo2 = document.getElementById('photot')
+photo2.src = img2[num2];
+
+function next2() {
+  num2 += 1
+  if (num2 >= img2.length) {
+    num2 = 0
+  }
+  photo2.src = img2[num2];
+  console.log('Next ok')
+}
+
+function prev2() {
+  num2 -= 1
+  if (num2 < 0) {
+    num2 = img2.length - 1
+  }
+  photo2.src = img2[num2];
+  console.log("Prev ok")
+}
+
+function sum() {
+  var n1 = prompt("1")
+  n1 = parseInt(n1)
+  var n2 = prompt("2")
+  n2 = parseInt(n2)
+  var sum = n1 + n2
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+sum+"}"
+  if (sum == "NaN") {
+    sum = "You should only input numbers"
+  }
+}
+
+function sub() {
+  var n1 = prompt("1")
+  n1 = parseInt(n1)
+  var n2 = prompt("2")
+  n2 = parseInt(n2)
+  var sum = n1 - n2
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+sum+"}"
+}
+
+function mult() {
+  var n1 = prompt("1")
+  var n2 = prompt("2")
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+n1 * n2+"}"
+}
+
+function div() {
+  var n1 = prompt("1")
+  n1 = parseInt(n1)
+  var n2 = prompt("2")
+  n2 = parseInt(n2)
+  var sum = n1 / n2
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+sum+"}"
+}
+
+function bytwo() {
+  var n1 = prompt("1")
+  n1 = parseInt(n1)
+  var n2 = prompt("2")
+  n2 = parseInt(n2)
+  var by2 = Math.pow(n1, n2)
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+by2+"}"
+}
+
+function sqrt() {
+  var n1 = prompt("1")
+  n1 = parseInt(n1)
+  var by2 = Math.sqrt(n1)
+  let nr = document.getElementById('nr')
+  nr.innerHTML = "{"+by2+"}"
+}
+
+function cleare() {
+  let nr = document.getElementById(`nr`);
+  nr.innerHTML = "{-}"
+}
+
+addEventListener('load', function(e) {
+  var d = new Date;
+  var day = d.getDay();
+  var dd = d.getDay();
+  dd += 1
+  if (day==0) {
+    day="Sunday"
+  } else if (day==1) {
+    day='Monday'
+  } else if (day==2) {
+    day='Tuesday'
+  } else if (day==3) {
+    day='Wednesday'
+  } else if (day==4) {
+    day='Thursday'
+  } else if (day==5) {
+    day='Friday'
+  } else {
+    day='Saturday'
+  };
+  var mday = d.getDate();
+  var month = d.getMonth();
+  month += 1;
+  var year = d.getYear();
+  year += 1900;
+    if (month==1) {
+    month="January"
+  } else if (month==2) {
+    month='February'
+  } else if (month==3) {
+    month='March'
+  } else if (month==4) {
+    month='April'
+  } else if (month==5) {
+    month='May'
+  } else if (month==6) {
+    month='June'
+  } else if (month==7) {
+    month='July'
+  } else if (month==8) {
+    month='August'
+  } else if (month==9) {
+    month='September'
+  } else if (month==10) {
+    month='October'
+  } else if (month==11) {
+    month='November'
+  } else if (month==12) {
+    month='December'
+  } else {
+    month='N/A'
+  };
+document.querySelector('#lol').innerHTML = day+", "+month+" "+mday+", "+year;
+});
