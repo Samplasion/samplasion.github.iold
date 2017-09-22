5fc5466e46545e9894519e5bbb5124a4fd268a23
Samplasion <ercolino2004@gmail.com>
2017-09-22 16:04:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,12 @@ function random() {
 function hex() {
   var nrs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
   var rand = nrs[Math.floor(Math.random() * nrs.length)];
-  document.getElementById("randex").innerHTML = '<font color="#'+rand+rand+rand+rand+rand+rand+'">#'+rand+rand+rand+rand+rand+rand+' is the color</font>'
+    var rand2 = nrs[Math.floor(Math.random() * nrs.length)];
+    var rand3 = nrs[Math.floor(Math.random() * nrs.length)];
+    var rand4 = nrs[Math.floor(Math.random() * nrs.length)];
+    var rand5 = nrs[Math.floor(Math.random() * nrs.length)];
+    var rand6 = nrs[Math.floor(Math.random() * nrs.length)];
+  document.getElementById("randex").innerHTML = '<font color="#'+rand+rand2+rand3+rand4+rand5+rand6+'">#'+rand+rand2+rand3+rand4+rand5+rand6+' is the color</font>'
 }
 
 var img = [
