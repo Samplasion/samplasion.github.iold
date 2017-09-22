4e3a4bf88658b46a8d6d5fbb3c8292d478b8dd9b
Samplasion <ercolino2004@gmail.com>
2017-09-22 15:13:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,8 @@ function random() {
 function hex() {
   var nrs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
   var rand = nrs[Math.floor(Math.random() * nrs.length)];
-  document.getElementById("randex").innerHTML = '<font color="#'+rand+rand+rand+rand+rand+rand+'">#'+rand+rand+rand+rand+rand+rand+'is the color</font>'
+  document.getElementById("randex").innerHTML = '<font color="#'+rand+rand+rand+rand+rand+rand+'">#'+rand+rand+rand+rand+rand+rand+' is the color</font>'
+}
 
 var img = [
   'img/trumpban.jpg',
