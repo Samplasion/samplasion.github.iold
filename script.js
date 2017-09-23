7d742d88bf6857c900b680ccf73e1f9e46ee7dfc
Samplasion <ercolino2004@gmail.com>
2017-09-23 18:57:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -93,30 +93,29 @@ function move() {
     }
 }
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+var slider = document.getElementById("myRange");
+var output = document.getElementById("out");
+var value = slider.value
+if (value == 0) {
+      value = "Random"
+    } else if (value == 1) {
+      value = "Even"
+    } else {
+      value = "Odd"
     }
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    var value = slider.value
+    if (value == 0) {
+      value = "Random"
+    } else if (value == 1) {
+      value = "Even"
+    } else {
+      value = "Odd"
+    }
+    output.innerHTML = this.value;
 }
 
 function hex() {
