1fad697724b67bc5005a44d1c8d8125608a279cc
Samplasion <ercolino2004@gmail.com>
2017-09-23 13:21:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -78,13 +78,13 @@ function fib() {
 
 function move() {
     var elem = document.getElementById("myBar"); 
-    var width = 0.5;
+    var width = 0.1;
     var id = setInterval(frame, 10);
     function frame() {
         if (width >= 100) {
             clearInterval(id);
         } else {
-            width+=0.5; 
+            width+=0.1; 
             elem.style.width = width + '%'; 
             elem.innerHTML = width * 1 + '%';
         }
