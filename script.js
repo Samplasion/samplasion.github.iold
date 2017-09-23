66cdbc21e60a1340ce34992d95f73345a0de9c79
Samplasion <ercolino2004@gmail.com>
2017-09-23 18:36:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -93,6 +93,32 @@ function move() {
     }
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function hex() {
   var nrs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
   var rand = nrs[Math.floor(Math.random() * nrs.length)];
