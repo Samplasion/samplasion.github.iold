2b5712ea737055d1534366e06dfea784741e8603
Samplasion <ercolino2004@gmail.com>
2017-09-24 15:54:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
 addEventListener('load', function(e) {
  var deg = Math.PI / 180
- function Data() {
-   var d = new Date;
-   var day = d.getDay();
-   var dd = d.getDay();
-   dd += 1
-   if (day==0) {
-     day="Sunday"
-   } else if (day==1) {
-     day='Monday'
-   } else if (day==2) {
-     day='Tuesday'
-   } else if (day==3) {
-     day='Wednesday'
-   } else if (day==4) {
-     day='Thursday'
-   } else if (day==5) {
-     day='Friday'
-   } else {
-     day='Saturday'
-   };
-   var mday = d.getDate();
-   var month = d.getMonth();
-   month += 1;
-   var year = d.getYear();
-   year += 1900;
-   var yr = d.getYear();
-   yr -= 100
-   var hours = d.getHours();
-   var mins = d.getMinutes();
-   var secs = d.getSeconds();
-   if (secs < 10) {
-     secs = '0'+secs
-   }
-   if (mins < 10) {
-     mins = '0'+mins
-   }
-   if (hours < 10) {
-     hours = '0'+hours
-   }
-   if (month < 10) {
-     month = '0'+month
-   };
- document.getElementById('newd').innerHTML =  hours+":"+mins+":"+secs+' '+day+', '+month+'/'+mday+'/'+year+" ('"+yr+')';
- }
- function data() {
-   setInterval(Data, 1000)
- }
- 
  function getRndInteger(min, max) {
      return Math.floor(Math.random() * (max - min + 1) ) + min;
  }
@@ -61,25 +13,7 @@ addEventListener('load', function(e) {
    var ll = document.getElementById('random')
    ll.innerHTML = 'Your random number is <font color="#af5000">' + lol + '</font> and is ' + evenodd
  }
- 
- 
-function move() {
-     var elem = document.getElementById("myBar"); 
-     var width = 1;
-     var id = setInterval(frame, 10);
-      function frame() {
-          if (width >= 100) {
-              clearInterval(id);
-            var butt = document.getElementById('prob')
-            butt.parentNode.removeChild(butt)
-          } else {
-              width++; 
-              elem.style.width = width + '%'; 
-             elem.innerHTML = width * 1 + '%';
-         }
-     }
- }
- 
+
  function hex() {
    var nrs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
    var rand = nrs[Math.floor(Math.random() * nrs.length)];
@@ -153,59 +87,4 @@ function move() {
    let nr = document.getElementById(`nr`);
    nr.innerHTML = "{-}"
  }
-
-function loadere() {
-   var d = new Date;
-   var day = d.getDay();
-   var dd = d.getDay();
-   dd += 1
-   if (day==0) {
-     day="Sunday"
-   } else if (day==1) {
-     day='Monday'
-   } else if (day==2) {
-     day='Tuesday'
-   } else if (day==3) {
-     day='Wednesday'
-   } else if (day==4) {
-     day='Thursday'
-   } else if (day==5) {
-     day='Friday'
-   } else {
-     day='Saturday'
-   };
-   var mday = d.getDate();
-   var month = d.getMonth();
-   month += 1;
-   var year = d.getYear();
-   year += 1900;
-     if (month==1) {
-     month="January"
-   } else if (month==2) {
-     month='February'
-   } else if (month==3) {
-     month='March'
-   } else if (month==4) {
-     month='April'
-   } else if (month==5) {
-     month='May'
-   } else if (month==6) {
-     month='June'
-   } else if (month==7) {
-     month='July'
-   } else if (month==8) {
-     month='August'
-   } else if (month==9) {
-     month='September'
-   } else if (month==10) {
-     month='October'
-   } else if (month==11) {
-     month='November'
-   } else if (month==12) {
-     month='December'
-   } else {
-     month='N/A'
-   };
- document.getElementById('lol').innerHTML = day+", "+month+" "+mday+", "+year;
-}
 });
